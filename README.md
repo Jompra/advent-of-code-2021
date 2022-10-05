e016e6c861588d5f4eeea8c36c80c67e00ad51e7
George Jones <george@circleaudiovisual.com>
2022-10-05 15:34:15 +0100

    day 1 puzzle 1 complete

---
+++ README.md
@@ -0,0 +1,3 @@
+This is just my repo for advent of code 2021 solutions. They might be the same as yours, they may be different, eitehr way they're mine.
+
+For the most part, I haven't implemented any testing, as I am just having fun with these. Some of the more complex solutions may include testing.
---
+++ day_1/solution.py
@@ -0,0 +1,19 @@
+
+def main():
+    increases = 0
+
+    with open('./day_1/input.txt', 'r') as f:
+        input_text = f.read()
+    
+    report_nums = input_text.split('\n')
+
+    for i in range(len(report_nums)):
+
+        if int(report_nums[i]) > int(report_nums[i - 1]):
+            increases += 1
+
+    print(f'There are {increases} measurements that are larger than the previous measurement')
+        
+
+if __name__ == '__main__':
+    main()
