3dfacf91539b1f90ff8a6162b751e1743a9e4bcb
George Jones <george@circleaudiovisual.com>
2022-10-05 16:37:12 +0100

    day 2 puzzle 2 complete

---
+++ day_2/puzzle_1/solution.py
@@ -0,0 +1,25 @@
+def main():
+    with open('./day_2/puzzle_1/input.txt', 'r') as f:
+        input_text = f.read()
+        instructions = input_text.split('\n')
+    
+    depth = 0
+    h_pos = 0
+
+    for instruction in instructions:
+        split_instruction = instruction.split(' ')
+        if split_instruction[0] == 'forward':
+            h_pos += int(split_instruction[1])
+        if split_instruction[0] == 'down':
+            depth += int(split_instruction[1])
+        if split_instruction[0] == 'up':
+            depth -= int(split_instruction[1])
+    print(f'The Depth of the submarine is {depth}')
+    print(f'The horizontal position of the submarine is {h_pos}')
+    print(f'the final position of the submarine is {depth * h_pos}')
+
+
+        
+
+if __name__ == '__main__':
+    main()
